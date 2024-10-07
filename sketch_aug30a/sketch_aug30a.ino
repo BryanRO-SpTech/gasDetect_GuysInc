f233520ea0bd08469fb0dfcdcf12eac25034e9e8
BryanRO-SpTech <bryan.grocha@sptech.school>
2024-10-07 09:59:23 -0300

    Dataquino

--- sketch_aug30a/sketch_aug30a.ino
+++ sketch_aug30a/sketch_aug30a.ino
@@ -1,5 +1,5 @@
 //Aqui declaramos uma constante (é tipo uma variável, só que você apenas atribui o valor dela uma vez), que está definida para ler a Entrada Analógica A0
-const int PINO_SENSOR_MQ2 = A5;
+const int PINO_SENSOR_MQ2 = A0;
 //Aqui declaramos uma constante de valor minimo para ser usado no calculo
 const int VALOR_MINIMO = 100;
 //Aqui declaramos uma constante de valor máximo para ser usado no calculo
