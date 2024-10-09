15cc48fa5484f7e22846094c4b185f07125c6441
Bryan-ro <bryangomesrocha@gmail.com>
2024-10-08 22:06:49 -0300

    Atualização de validação na tela de cadastro

--- Site_Institucional/js/validarCadastro.js
+++ Site_Institucional/js/validarCadastro.js
@@ -1,90 +1,100 @@
+let razaoSocialValido = false;
+let cnpjValido = false;
+let nomeValido = false;
+let emailValido = false;
+let cpfValido = false;
+let senhaValido = false;
+let confirmarSenhaValido = false;
+
+
+
 function validarRazaoSocial() {
+    razaoSocialValido = false
+
     const razaoSocial = document.getElementById("ipt_razaoSocial").value;
     const spanMensagem = document.getElementById("span_mensagem_razao_social");
 
     let mensagem = "";
-    let validado = true;
 
     if (razaoSocial.length == 0) {
         mensagem = "A razão social não pode ser vazia.";
-        validado = false;
     }
 
     else if (razaoSocial.length > 60) {
         mensagem = "A razão social não pode ter mais de 60 caracteres.";
-        validado = false;
+    }
+
+    else {
+        razaoSocialValido = true;
     }
 
     spanMensagem.innerHTML = mensagem;
-
-    return validado;
 }
 
 function validarCnpj() {
-    const cnpj = document.getElementById("ipt_cnpj").value;
+    cnpjValido = false
+
+    const cnpj = document.getElementById("ipt_cnpj");
     const spanMensagem = document.getElementById("span_mensagem_cnpj");
 
     let mensagem = "";
-    let validado = true;
 
-    if (!contemApenasNumeros(cnpj)) {
-        mensagem = "CNPJ deve conter apenas números.";
-        validado = false;
+    if (!contemApenasNumeros(cnpj.value)) {
+        removerCaracteresInvalidos(cnpj, "0123456789")
     }
 
-    else if (cnpj.length != 14) {
-        mensagem = "CNPJ deve conter exatamente 14 dígitos.";
-        validado = false;
+    if (cnpj.value.length < 14) {
+        mensagem = `CNPJ deve conter exatamente 14 dígitos. Digite mais ${14 - cnpj.value.length} números...`;
+    }
+
+    else {
+        cnpjValido = true
     }
 
     spanMensagem.innerText = mensagem;
-
-    return validado;
 }
 
 function validarNome() {
+    nomeValido = false;
+
     const nome = document.getElementById("ipt_nome").value;
     const spanMensagem = document.getElementById("span_mensagem_nome");
 
     let mensagem = "";
-    let validado = true;
 
     if (nome.length === 0) {
         mensagem = "O nome não pode ser vazio.";
-        validado = false;
     }
 
     else if (nome.length > 45) {
         mensagem = "O nome não pode ter mais de 45 caracteres.";
-        validado = false;
     }
 
     else if (contemNumeros(nome) || contemCaracterEspecial(nome)) {
         mensagem = "O nome não pode conter números, nem caracteres especiais.";
-        validado = false;
+    }
+
+    else {
+        nomeValido = true;
     }
 
     spanMensagem.innerText = mensagem;
-
-    return validado;
 }
 
 function validarEmail() {
+    emailValido = false;
+
     const email = document.getElementById("ipt_email").value;
     const spanMensagem = document.getElementById("span_mensagem_email");
 
     let mensagem = "";
-    let validado = true;
-
 
     if (email.length === 0) {
         mensagem = "O e-mail não pode ser vazio.";
-        validado = false;
     }
 
     else if (email.length > 60) {
         mensagem = "O e-mail não pode ter mais de 60 caracteres.";
-        validado = false;
     }
 
     else if (
@@ -96,120 +106,99 @@ function validarEmail() {
         email.indexOf("@") + 1 === email.lastIndexOf(".")
     ) {
         mensagem = "O formato do e-mail é inválido.";
-        validado = false;
+    }
+
+    else {
+        emailValido = true;
     }
 
 
     spanMensagem.innerHTML = mensagem;
-    return validado;
 }
 
 
 function validarCpf() {
-    const cpf = document.getElementById("ipt_cpf").value;
+    cpfValido = false;
+
+    const cpf = document.getElementById("ipt_cpf");
     const spanMensagem = document.getElementById("span_mensagem_cpf");
 
     let mensagem = "";
-    let validado = true;
 
-    if (!contemApenasNumeros(cpf)) {
-        mensagem = "CPF deve conter apenas números.";
-        validado = false;
+    if (!contemApenasNumeros(cpf.value.value)) {
+        removerCaracteresInvalidos(cpf, "0123456789")
     }
 
-    else if (cpf.length !== 11) {
-        mensagem = "CPF deve conter exatamente 11 dígitos.";
-        validado = false;
+    if (cpf.value.length < 11) {
+        mensagem = `CPF deve conter exatamente 11 dígitos. Digite mais ${11 - cpf.value.length} números...`;
+    }
+
+    else {
+        cpfValido = true;
     }
 
     spanMensagem.innerHTML = mensagem;
-    return validado;
 }
 
 function validarSenha() {
+    senhaValido = false;
+
     const senha = document.getElementById("ipt_senha").value;
     const spanMensagem = document.getElementById("span_mensagem_senha");
 
     let mensagem = "";
-    let validado = true;
 
 
     if (!contemNumeros(senha) || !contemLetraMaiuscula(senha) || !contemLetraMinuscula(senha) || !contemCaracterEspecial(senha)) {
         mensagem = "A senha deve conter pelo menos uma letra maiúscula, uma letra minúscula, um número e um caracter especial.";
-        validado = false;
     }
 
     else if (senha.length < 8 || senha.length > 30) {
         mensagem = "A senha deve ter entre 8 e 30 caracteres.";
-        validado = false;
+    }
+
+    else {
+        senhaValido = true;
     }
 
 
 
     spanMensagem.innerHTML = mensagem;
-    return validado;
 }
 
 function validarConfirmarSenha() {
+    confirmarSenhaValido = false;
+
     const senha = document.getElementById("ipt_senha").value;
     const confirmarSenha = document.getElementById("ipt_confirmar").value;
     const spanMensagem = document.getElementById("span_mensagem_confirmar");
 
     let mensagem = "";
-    let validado = true;
 
     if (senha !== confirmarSenha) {
         mensagem = "As senhas não coincidem.";
-        validado = false;
+    }
+
+    else {
+        confirmarSenhaValido = true;
     }
 
     spanMensagem.innerHTML = mensagem;
-    return validado;
 }
 
-function ativarBotao() {
-    const botaoCadastrar = document.getElementById("btn_cadastrar");
-
-    validarRazaoSocial() &&
-        validarCnpj() &&
-        validarNome() &&
-        validarEmail() &&
-        validarCpf() &&
-        validarSenha() &&
-        validarConfirmarSenha() ? botaoCadastrar.disabled = false : botaoCadastrar.disabled = true;
-}
-
-function removerCaracteresInvalidos() {
-    const removerCaractere = (input, caracteresValidos) => {
-        let texto = "";
-        for (let i = 0; i < input.value.length; i++) {
-            if (caracteresValidos.includes(input.value[i])) {
-                texto += input.value[i];
-            }
-        }
-
-        input.value = texto;
+function redirecionar() {
+    if (
+        razaoSocialValido &&
+        cnpjValido &&
+        nomeValido &&
+        emailValido &&
+        cpfValido &&
+        senhaValido &&
+        confirmarSenhaValido
+    ) {
+        window.location.replace("./login.html");
     }
-
-
-    const cnpj = document.getElementById("ipt_cnpj");
-    const cpf = document.getElementById("ipt_cpf");
-
-    removerCaractere(cnpj, "1234567890");
-    removerCaractere(cpf, "1234567890");
 }
-
-
-const inputs = document.querySelectorAll("#cadastro_form input");
-
-for (let i = 0; i < inputs.length; i++) {
-    inputs[i].addEventListener("input", function () {
-        removerCaracteresInvalidos();
-        ativarBotao();
-    });
-}
-
-
 
 
 
@@ -267,6 +256,13 @@ function contemCaracterEspecial(texto) {
     return false;
 }
 
-function removerValoresInvalidos(input, caracteresValidos) {
+function removerCaracteresInvalidos(input, caracteresValidos) {
+    let texto = "";
+    for (let i = 0; i < input.value.length; i++) {
+        if (caracteresValidos.includes(input.value[i])) {
+            texto += input.value[i];
+        }
+    }
 
+    input.value = texto;
 }
