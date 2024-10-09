802c09d66da37edd4075441d3d08a0cbe48d32e0
BryanRO-SpTech <bryan.grocha@sptech.school>
2024-10-09 16:00:15 -0300

    Cadastro responsivo

--- Site_Institucional/js/validarLogin.js
+++ Site_Institucional/js/validarLogin.js
@@ -1,4 +1,12 @@
 function validarLogin() {
-    const email = document.getElementById("ipt_email");
-    const senha = document.getElementById("ipt_senha");
+    const email = document.getElementById("ipt_email").value;
+    const senha = document.getElementById("ipt_senha").value;
+    const spanError = document.getElementById("spn_login_error");
+
+    if (email != "guys@guysinc.com" || senha != "@Urubu100") {
+        spanError.innerHTML = "E-mail ou senha incorretos, tente novamente.";
+    } else {
+        window.location.replace("./dashboard.html");
+    }
+
 }
