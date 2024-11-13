968358997d666625223d59b9ca34af42347248e5
BryanRO-SpTech <bryan.grocha@sptech.school>
2024-11-13 13:57:28 -0300

    correção de bug no cadastro

--- web-data-viz/src/models/usuarioModel.js
+++ web-data-viz/src/models/usuarioModel.js
@@ -12,18 +12,18 @@ function autenticar(email, senha) {
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
 async function cadastrar(razaoSocial, cnpj, nome, email, cpf, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", razaoSocial, cnpj, nome, email, cpf, senha);
-    
+
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucaoSqlempresa = `
         INSERT INTO Empresa (razaoSocial, cnpjSede) VALUES ('${razaoSocial}', '${cnpj}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSqlempresa);
-    
+
     const empresa = await database.executar(instrucaoSqlempresa);
-    
+
     var instrucaoSqlusuario = `
-    INSERT INTO usuario (nome, email, cpf, senha, fkEmpresa) VALUES ('${razaoSocial}', '${cnpj}', '${nome}', '${email}', '${cpf}', '${senha}', '${empresa.id}');
+        INSERT INTO Funcionario (nome, email, cpf, senha, fkEmpresa) VALUES ('${nome}', '${email}', '${cpf}', '${senha}', '${empresa.insertId}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSqlempresa);
     return database.executar(instrucaoSqlusuario);
