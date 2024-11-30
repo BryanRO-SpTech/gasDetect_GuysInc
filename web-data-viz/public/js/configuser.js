b8f2c1ce5202d3cb4147f2adfe62460581ba5860
Victorsilva13 <victor.slima@sptech>
2024-11-29 23:51:13 -0300

    atualização backend nas telas de configuração de usuario

--- web-data-viz/public/js/configuser.js
+++ web-data-viz/public/js/configuser.js
@@ -2,20 +2,34 @@ function mostrardadosuser() {
     const username = sessionStorage.getItem('NOME')
     const useremail = sessionStorage.getItem('EMAIL')
     const usercpf = sessionStorage.getItem('CPF')
-    const userlevel = sessionStorage.getItem('NIVEL_PERMISSAO')
-
+    
     document.getElementById('ipt_nome').placeholder = username
     document.getElementById('ipt_email').placeholder = useremail
     document.getElementById('ipt_cpf').placeholder = usercpf
-
+    
     b_usuario.innerHTML = username;
     a_usuario.innerHTML = username;
+}
 
-    if (userlevel == 1) {
+function niveladministrador() {
+    const userlevel = sessionStorage.getItem('NIVEL_PERMISSAO')
+    if (userlevel == '1') {
         document.getElementById('condicional').style.display = 'flex'
     }
 }
 
-function alterar_senha() {}
+function alterar_senha() {
+    var username = ipt_nome.value
+    var useremail = ipt_email.value
+    var usercpf = ipt_cpf.value
+    var useroriginalsenha = ipt_senha_original.value
+    var usersenha = ipt_senha.value
+    var userconfirmsenha = ipt_confirmar.value
+}
 
-function salvar() {}
+function salvar() {
+    var username = ipt_nome.value
+    var useremail = ipt_email.value
+    var usercpf = ipt_cpf.value
+    var useroriginalsenha = ipt_senha_original.value
+}
